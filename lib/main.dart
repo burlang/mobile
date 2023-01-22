7fb6238bd3b76aed4dd621a86d890537883d4825
Thebubeev <bubeevm@gmail.com>
2023-01-23 04:51:34 +0800

    added http cert client

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:burlang_demo/bloc/burlang_bloc.dart';
 import 'package:burlang_demo/screens/splash_start_screen.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +7,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'config/router.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -17,8 +29,8 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [BlocProvider<BurlangBloc>(create: (_) => BurlangBloc())],
       child: const MaterialApp(
+          title: 'Burlang',
           debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
           onGenerateRoute: RouteGenerator.generateRoute,
           home: SplashStartScreen()),
     );
